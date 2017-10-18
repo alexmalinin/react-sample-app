3e8d6cec448bef2d8f7fd5400bb460300ca72a91
kohut <denkohut.codica@gmail.com>
2017-10-18 18:44:54 +0300

    init markdown

--- src/components/PostProject/forms/DetailsSelects.js
+++ src/components/PostProject/forms/DetailsSelects.js
@@ -11,11 +11,30 @@ import RenderCheckbox from '../../forms/renders/RenderCheckbox';
 import SlideTogle from '../../SlideTogle';
 import { required, } from '../../../helpers/validate';
 import InputField from "../../forms/renders/InputField";
+import RichTextEditor from 'react-rte';
 
 class DetailsSelects extends Component {
 
+    state = {
+        value: RichTextEditor.createEmptyValue()
+    };
+
     render() {
+        window.value = this.state.value.toString('html');
+
         const { handleSubmit, submitting } = this.props;
+        const toolbarConfig = {
+            // Optionally specify the groups to display (displayed in the order listed).
+            display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS',],
+            INLINE_STYLE_BUTTONS: [
+                {label: 'Bold', style: 'BOLD', className: 'custom-css-class'},
+                {label: 'Italic', style: 'ITALIC'},
+            ],
+            BLOCK_TYPE_BUTTONS: [
+                {label: 'UL', style: 'unordered-list-item'},
+                {label: 'OL', style: 'ordered-list-item'}
+            ]
+        };
 
         return(
             <div>
@@ -47,9 +66,28 @@ class DetailsSelects extends Component {
                     options={remotes}
                     validate={[required]}
                 />
+
+                <RichTextEditor
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    toolbarConfig={toolbarConfig}
+                />
             </div>
         )
     }
+
+    onChange = (value) => {
+
+        if (this.props.onChange) {
+            // Send the changes up to the parent component as an HTML string.
+            // This is here to demonstrate using `.toString()` but in a real app it
+            // would be better to avoid generating a string on each change.
+            this.props.onChange(
+                value.toString('html')
+            );
+        }
+        this.setState({value});
+    };
 }
 
 export default DetailsSelects
