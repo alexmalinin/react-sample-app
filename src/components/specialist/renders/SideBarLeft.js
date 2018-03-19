51e9cbeb7cdfe4eeca38cca7174fd6a88e264843
Alex Malinin <amalinin.codica@gmail.com>
2018-03-19 18:13:37 +0200

    [feature] Add left and right sidebars

---
+++ src/components/specialist/renders/SideBarLeft.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { StyledBar } from '../../../styleComponents/layout/SideBar';
+import { Accordion } from 'semantic-ui-react';
+
+export default class SideBarLeft extends Component {
+
+    render() {
+        let { projects } = this.props;
+
+        return(
+            <StyledBar className="left" >
+                <Accordion activeIndex={0}>
+                    <Accordion.Title index={0}>
+                        Projects
+                    </Accordion.Title>
+                    <Accordion.Content>
+                        
+                        <Accordion>
+                            <Accordion.Title index={0}>
+                                ABC
+                            </Accordion.Title>
+                            <Accordion.Content>
+                                <ul>
+                                    <li><a href="#">ABC content 1</a></li>
+                                    <li><a href="#">ABC content 2</a></li>
+                                    <li><a href="#">ABC content 3</a></li>
+                                </ul>
+                            </Accordion.Content>
+                            <Accordion.Title index={1}>
+                                XYZ
+                            </Accordion.Title>
+                            <Accordion.Content>
+                                <ul>
+                                    <li><a href="#">XYZ content 1</a></li>
+                                    <li><a href="#">XYZ content 2</a></li>
+                                    <li><a href="#">XYZ content 3</a></li>
+                                </ul>
+                            </Accordion.Content>
+                        </Accordion>
+                        {/* <Accordion>
+                            {projects.map((project, index) => 
+                                <div>
+                                    <Accordion.Title index={index}>
+                                        {project.name}
+                                    </Accordion.Title>
+                                    <Accordion.Content>
+                                        <ul>
+                                            {project.subpoints.map((value, index) => <li key={index}><a href='#'>{value}</a></li>)}
+                                        </ul>
+                                    </Accordion.Content>
+                                </div>
+                            )}
+                        </Accordion> */}
+
+                    </Accordion.Content>
+                </Accordion>
+            </StyledBar>
+        );
+    }
+}
