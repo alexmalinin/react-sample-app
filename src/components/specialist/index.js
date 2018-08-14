54b99bf9d31aa8d6870fd00f1f7171bd789ef918
Alex Malinin <amalinin.codica@gmail.com>
2018-08-14 19:54:18 +0300

    [Feature] remove company and billings from about page

--- src/components/specialist/index.js
+++ src/components/specialist/index.js
@@ -444,9 +444,6 @@ class SpecialistsDashboard extends Component {
             collectBillingData={this.collectBillingData}
           />
         );
-      case "edit":
-        document.title = "Edit profile | Digital Village";
-        return <EditProfile />;
       case "projects":
         document.title = "Add Project | Digital Village";
         return <ClientProjects />;
