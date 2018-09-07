d58e5c0ffe24d62d4e781ab91f1ffdda14a4273b
Yura Korovko <ykorovko.codica@gmail.com>
2018-09-07 13:41:36 +0300

    [Fix] scrolling to the top on all pages

--- src/views/components/Profile/Info/InfoContainer.js
+++ src/views/components/Profile/Info/InfoContainer.js
@@ -4,25 +4,39 @@ import { reduxForm } from "redux-form";
 import { getUserData } from "@ducks/user/actions";
 import { updateUserProfile } from "@ducks/profile/actions";
 import { showSubmitErrorModal } from "@ducks/modals/actions";
+import { isSpecialist } from "@ducks/user/selectors";
 
 import Info from "./Info";
 
-const mapStateToProps = ({ user, profile: { info } }) => ({
-  avatar: info.avatar,
-  userRole: user.role,
-  initialValues: {
-    first_name: info.first_name,
-    last_name: info.last_name,
-    email: info.email,
-    city: info.address && info.address.city,
-    country: info.address && info.address.country,
-    phone_number: info.phone_number,
-    description: info.description,
-    professional_experience_info: info.professional_experience_info
-  },
-  educations: info.educations,
-  work_experiences: info.work_experiences
-});
+const makeMapStateToProps = () => {
+  const getSpecialist = isSpecialist();
+
+  const mapStateToProps = (state, props) => {
+    const {
+      user,
+      profile: { info }
+    } = state;
+
+    return {
+      avatar: info.avatar,
+      isSpecialist: getSpecialist(user),
+      initialValues: {
+        first_name: info.first_name,
+        last_name: info.last_name,
+        email: info.email,
+        city: info.address && info.address.city,
+        country: info.address && info.address.country,
+        phone_number: info.phone_number,
+        description: info.description,
+        professional_experience_info: info.professional_experience_info
+      },
+      educations: info.educations,
+      work_experiences: info.work_experiences
+    };
+  };
+
+  return mapStateToProps;
+};
 
 const mapDispatchToProps = {
   getUserData,
@@ -42,4 +56,4 @@ const withForm = reduxForm({
   }
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(withForm(Info));
+export default connect(makeMapStateToProps, mapDispatchToProps)(withForm(Info));
--- src/views/routes/index.js
+++ src/views/routes/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import { animateScroll as scroll } from "react-scroll";
 
 import signIn from "./signIn";
 import signUp from "./signUp";
@@ -17,44 +18,59 @@ import PrivateRoute from "../utils/hoc/PrivateRoute";
 import GuestRoute from "../utils/hoc/GuestRoute";
 import RedirectRoute from "../utils/decorators/RedirectRoute";
 import AssignRoute from "../utils/decorators/AssignRoute";
+import history from "../../history";
 
 import FlexDirection from "@styled/FlexDirection";
+import { getAllUrlParams } from "@views/utils/functions";
 
-export default () => (
-  <FlexDirection>
-    <Switch>
-      <Route exact path="/" component={defaultRoute} />
-      <Route exact path="/index.html" component={defaultRoute} />
-      <GuestRoute exact path="/sign_in" component={signIn} />
-      <GuestRoute path="/sign_up" component={signUp} />
-      <GuestRoute path="/confirm_email" component={confirmation} />
-      <GuestRoute path="/forgot_password" component={forgotPassword} />
-      <GuestRoute path="/confirm_reset" component={confirmReset} />
-      <GuestRoute path="/reset_password" component={resetPassword} />
-      <GuestRoute path="/create_password" component={createPassword} />
-      <PrivateRoute path="/profile" component={profile} />
-      <PrivateRoute path="/dashboard" component={dashboard} />
+class App extends Component {
+  componentDidMount() {
+    history.listen(location => {
+      const param = getAllUrlParams(location.search).hash;
+      if (!param) scroll.scrollToTop(500);
+    });
+  }
 
-      <AssignRoute
-        path="/api/v1/projects/:projectId/teams/:teamId/assign/:specialistId"
-        assignPath="/dashboard/project/:projectId"
-      />
+  render() {
+    return (
+      <FlexDirection>
+        <Switch>
+          <Route exact path="/" component={defaultRoute} />
+          <Route exact path="/index.html" component={defaultRoute} />
+          <GuestRoute exact path="/sign_in" component={signIn} />
+          <GuestRoute path="/sign_up" component={signUp} />
+          <GuestRoute path="/confirm_email" component={confirmation} />
+          <GuestRoute path="/forgot_password" component={forgotPassword} />
+          <GuestRoute path="/confirm_reset" component={confirmReset} />
+          <GuestRoute path="/reset_password" component={resetPassword} />
+          <GuestRoute path="/create_password" component={createPassword} />
+          <PrivateRoute path="/profile" component={profile} />
+          <PrivateRoute path="/dashboard" component={dashboard} />
 
-      <AssignRoute
-        path="/api/v1/teams/:teamId/assign/:specialistId"
-        assignPath="/dashboard/teams"
-      />
+          <AssignRoute
+            path="/api/v1/projects/:projectId/teams/:teamId/assign/:specialistId"
+            assignPath="/dashboard/project/:projectId"
+          />
 
-      <RedirectRoute
-        path="/api/v1/:user/confirmation/:token"
-        to="/create_password"
-      />
+          <AssignRoute
+            path="/api/v1/teams/:teamId/assign/:specialistId"
+            assignPath="/dashboard/teams"
+          />
 
-      <RedirectRoute
-        path="/api/v1/:user/password_reset/:token"
-        to="/reset_password"
-      />
-      <Route component={NotFound} />
-    </Switch>
-  </FlexDirection>
-);
+          <RedirectRoute
+            path="/api/v1/:user/confirmation/:token"
+            to="/create_password"
+          />
+
+          <RedirectRoute
+            path="/api/v1/:user/password_reset/:token"
+            to="/reset_password"
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </FlexDirection>
+    );
+  }
+}
+
+export default App;
